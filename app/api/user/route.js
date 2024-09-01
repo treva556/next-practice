16e177548a8735c90464af4f1407065a6bb431a2
treva556 <marktreva92@gmail.com>
2024-09-01 14:33:36 +0300

    milestone 4

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -2,14 +2,19 @@
 
 // app/api/user/page.js
 // app/api/user/route.js
-export async function GET() {
-    return new Response(
-      JSON.stringify({ name: 'Robert' }), 
-      {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-  }
+// app/api/user/route.js
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const query = Object.fromEntries(searchParams);
+
+  return new Response(
+    JSON.stringify({ query }), 
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+}
